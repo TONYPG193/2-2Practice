b45f6e7c96d1e1012d7635201d06beae90f2b990
TONYPG193 <74764919+TONYPG193@users.noreply.github.com>
2021-03-11 09:24:57 +0800

    Update Default.aspx.cs

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +8,21 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int year=8;
+            string result="Y";
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 != 0)
+                        result = "N";
+                }
+            }
+            else
+            {
+                result = "N";
+            }
+            Response.Write(result);
         }
     }
 }
